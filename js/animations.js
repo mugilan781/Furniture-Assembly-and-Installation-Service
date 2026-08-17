24d21077c7f9a16c3c6b5fd82d3a514fc97effac
Mugilan M <marvelmggladiator@gmail.com>
2026-08-17 23:16:42 +0530

    res7

--- js/animations.js
+++ js/animations.js
@@ -18,8 +18,8 @@
       }
     });
   }, {
-    threshold: 0.12,
-    rootMargin: '0px 0px -60px 0px'
+    threshold: 0.01,
+    rootMargin: '0px 0px -40px 0px'
   });
 
   elements.forEach(el => observer.observe(el));
